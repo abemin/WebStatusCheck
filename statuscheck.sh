4b851f86d0c362cae961fd022dd11dd686d4dec1
abemin <muhaimifatihi@hotmail.com>
2021-05-26 09:01:48 +0800

    Create statuscheck.sh

---
+++ statuscheck.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+WORKSPACE=/directory/where/script/
+# list of websites. each website in new line. leave an empty line in the end.
+LISTFILE=$WORKSPACE/websites.lst
+# Send mail in case of failure to. leave an empty line in the end.
+EMAILLISTFILE=$WORKSPACE/emails.lst
+# Temporary dir
+TEMPDIR=$WORKSPACE/cache
+
+# `Quiet` is true when in crontab; show output when it's run manually from shell.
+# Set THIS_IS_CRON=1 in the beginning of your crontab -e.
+# else you will get the output to your email every time
+if [ -n "$THIS_IS_CRON" ]; then QUIET=true; else QUIET=false; fi
+
+function test {
+  response=$(curl -L --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null $1)
+#  disable above and enable below for HTTPS
+#  response=$(curl -L --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null https://$1)
+  filename=$( echo $1 | cut -f1 -d"/" )
+  if [ "$QUIET" = false ] ; then echo -n "$p "; fi
+
+  if [ $response -eq 200 ] ; then
+    # website working
+    if [ "$QUIET" = false ] ; then
+      echo -n "$response "; echo -e "[ok]"
+    fi
+    # remove .temp file if exist 
+    if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
+  else
+    # website down
+    if [ "$QUIET" = false ] ; then echo -n "$response "; echo -e "[DOWN]"; fi
+    if [ ! -f $TEMPDIR/$filename ]; then
+        while read e; do
+            # using mailx command
+            echo "$p WEBSITE DOWN" | mailx -s "$1 WEBSITE DOWN ( $response )" $e
+            # using mail command
+            #mail -s "$p WEBSITE DOWN" "$EMAIL"
+        done < $EMAILLISTFILE
+        echo > $TEMPDIR/$filename
+    fi
+  fi
+}
+
+# main loop
+while read p; do
+  test $p
+done < $LISTFILE
